ec7fb47fc69ca3f104b7bc5fbc8d3b523374078b
Suryansh Gupta <suryanshg2680@gmail.com>
2026-05-30 16:57:21 +0530

    Update 8_Palindrome.cpp

--- Day_2/8_Palindrome.cpp
+++ Day_2/8_Palindrome.cpp
@@ -1,1 +1,23 @@
 //Check whether a number is palindrome 
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, c, rev=0;
+    cout << "Enter N: ";
+    cin >> n;
+
+    for(int x=n; x>0; x/=10){
+      c= x%10;  
+      rev = rev*10 + c;
+    }
+
+  if (rev==n)
+    cout << "The given number is Palindrome";
+  else
+   cout << "The given number is NOT Palindrome";
+
+    return 0;
+}
